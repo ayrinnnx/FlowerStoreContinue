38809b7825429ee15154468697e496716d70c99e
ayrinnnx <117468608+ayrinnnx@users.noreply.github.com>
2023-10-26 20:08:05 +0300

    Basics

---
+++ lab74/src/main/java/ua/edu/ucu/apps/lab74/flowers/Flower.java
@@ -0,0 +1,12 @@
+package ua.edu.ucu.apps.lab74.flowers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor @Getter
+public class Flower {
+    private double price;
+    private double sepalLength;
+    private FlowerColor color;
+    
+}
